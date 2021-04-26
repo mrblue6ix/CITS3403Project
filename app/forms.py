4bfe3d2555340cdca1a1b275eea9a4f1285dd6da
mrblue6ix <Jordan-moss1@hotmail.com>
2021-04-26 20:22:35 +0800

    added registration form to forms.py

--- app/forms.py
+++ app/forms.py
@@ -9,3 +9,10 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
