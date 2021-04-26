3143b82ae84cdcecd6171276e5ff2e8e4ade816a
mrblue6ix <Jordan-moss1@hotmail.com>
2021-04-26 20:34:04 +0800

    Added comments, and temp secret key

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+#change secret key to something else, it is unsafe to have it like below
+app.config['SECRET_KEY'] = 'this_is_a_temp_secret_key'
 from app import routes
