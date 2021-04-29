f99770f96a9afb81c9e2b7a6adadbc3e256378ec
mrblue6ix <Jordan-moss1@hotmail.com>
2021-04-29 18:19:57 +0800

    added db stuff to init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 # initialization file for the app
 from flask import Flask
+from config import config
+from flask_sqlalchemy import flask_sqlalchemy
+from flask_migrate import Migrate
 
+#add db stuff to config file to make this do stuff
 app = Flask(__name__)
-from app import routes
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from app import routes, models
