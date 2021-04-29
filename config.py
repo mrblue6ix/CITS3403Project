902d5a0d73608d0b7815d06661800494f5f7c3a6
mrblue6ix <Jordan-moss1@hotmail.com>
2021-04-29 19:49:11 +0800

    updated config

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ class Config(object):
     # export SECRET_KEY='this_is_the_secret_key'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     #also store database key, locations,... here
-    #should app.db be learnpython.db ????
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE.URL') or \
-        'sqlite:///' + os.path.join(basedir, 'learnpython.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
