e363baf7aecde38fff2d40f4fae716f933195fd8
calebcheng00 <50129617+calebcheng00@users.noreply.github.com>
2021-04-29 22:48:48 +0800

    created database migrations
    using flask-migrate

--- migrations/env.py
+++ migrations/env.py
@@ -10,7 +10,6 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
