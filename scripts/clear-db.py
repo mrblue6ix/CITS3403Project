51454062bff12542d58c15ba9f241c02d9564fbe
calebcheng00 <50129617+calebcheng00@users.noreply.github.com>
2021-05-07 00:21:27 +0800

    updated scripts !important!
    - questions are now written in markdown then formatted using python markdown library
    - clear-db script is more expressive
    - populate db takes prefill
    - populate db will transform to markdown

--- scripts/clear-db.py
+++ scripts/clear-db.py
@@ -9,19 +9,19 @@ import sys
 arguments = sys.argv
 if len(arguments) < 2:
     print("Incorrect number of arguments: python3 [app.db]")
-sure = input("Are you sure you want to delete the entire database? (y/n)")
-if sure != 'y':
-    exit()
 conn = sqlite3.connect(arguments[1])
 cursor = conn.cursor()
-cursor.execute("DELETE FROM Activity")
-cursor.execute("DELETE FROM ActivityDependency")
+if input("Do you want to delete the activities and their relationships? (y/n): ") == 'y':
+    cursor.execute("DELETE FROM Activity")
+    cursor.execute("DELETE FROM ActivityDependency")
 
-cursor.execute("DELETE FROM Module")
-cursor.execute("DELETE FROM ModuleDependency")
+if input("Do you want to delete the modules and their relationships? (y/n): ") == 'y':
+    cursor.execute("DELETE FROM Module")
+    cursor.execute("DELETE FROM ModuleDependency")
 
-cursor.execute("DELETE FROM User")
-cursor.execute("DELETE FROM UserActivity")
+if input("Do you want to delete the users and their relationships? (y/n): ") == 'y':
+    cursor.execute("DELETE FROM User")
+    cursor.execute("DELETE FROM UserActivity")
 
 conn.commit()
 conn.close()
--- scripts/populate-db.py
+++ scripts/populate-db.py
@@ -1,12 +1,9 @@
 import sqlite3
 import sys
 import yaml
-
-arguments = sys.argv
-if len(arguments) < 3:
-    print("Incorrect number of arguments: python3 [app.db] [modules.yaml]")
-conn = sqlite3.connect(arguments[1])
-cursor = conn.cursor()
+import markdown
+import os
+import glob
 
 # DIRECTORY STRUCTURE
 # resources/
@@ -17,34 +14,22 @@ cursor = conn.cursor()
 #       4-userinput.yaml
 #   2-numbers/
 #       1-numbers.yaml
+#   modules.yaml
 
-# CONTENTS OF modules.yaml file
-# ---
-# modules:
-# - name: 1-printing
-#   requires:
-#   title: Printing in Python
-#   description: For our first Python module, we will learn how to output to the terminal.
-# - name: 2-numbers
-#   requires:
-#     - 1-printing
-#   title: Numbers
-#   description: In this module, we will learn how to work with numbers in Python
-# - name: 3-strings
-#   requires:
-#     - 2-numbers
-#   title: Strings
-#   description: In this module, we will learn how to manipulate strings.
-# - name: 4-lists
-#   requires:
-#     - 3-strings
-#   title: Lists
-#   description: We will learn how to use lists.
-# ...
+arguments = sys.argv
+if len(arguments) < 3:
+    print("Incorrect number of arguments: python3 [app.db] [modules.yaml]")
+conn = sqlite3.connect(arguments[1])
+cursor = conn.cursor()
+
+
+def toMarkdown(activity):
+    if activity['prompt']:
+        activity['prompt'] = markdown.markdown(activity['prompt'], extensions=['nl2br'])
+    if activity['question']:
+        activity['question'] = markdown.markdown(activity['question'], extensions=['nl2br'])
+    return activity
             
-import yaml
-import os
-import glob
 # these are the folder to look in
 with open(arguments[2]) as modules:
     try:
@@ -98,11 +83,14 @@ for module in modules:
                 with open(activity_file) as activity:
                     try:
                         a = yaml.safe_load(activity)
+                        # Convert from Markdown to HTML
+                        a = toMarkdown(a)
                     except yaml.YAMLError as exc:
                         print(exc)
                 print(f"Inserting {module}/{a['name']}")
-                cursor.execute("INSERT INTO Activity(name,title, prompt, answer, solution, question, module_id) VALUES (?,?,?,?,?,?,?)",
-                                (a['name'], a['title'], a['prompt'], a['answer'], a['solution'], a['question'], module_id))
+                cursor.execute("INSERT INTO Activity(name,title, prompt, prefill, answer, solution, question, module_id) VALUES (?,?,?,?,?,?,?,?)",
+                                (a['name'], a['title'], a['prompt'], a['prefill'],
+                                 a['answer'], a['solution'], a['question'], module_id))
 print("Finished inserting Activities")
 
 # Insert activity dependencies
