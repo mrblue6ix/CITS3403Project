bc0f1dad7b03aee954f593c693189ecc3fe42063
calebcheng00 <50129617+calebcheng00@users.noreply.github.com>
2021-05-12 12:55:12 +0800

    temporarily removed functional test

--- tests/functional/test_dashboard.py
+++ tests/functional/test_dashboard.py
@@ -1,7 +1,7 @@
 from app import app
 
 from flask import Flask
-from config import Config
+from config import TestConfig
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -16,18 +16,23 @@ convention = {
     "pk": "pk_%(table_name)s"
 }
 
-def test_dashboard():
-    app = Flask(__name__)
-    login = LoginManager(app)
-
-    app.config.from_object(Config)
-    metadata = MetaData(naming_convention=convention)
-    db = SQLAlchemy(app, metadata=metadata)
-    migrate = Migrate(app, db)
-
-    # this is required as SQLite does not support ALTER of constraints
-    with app.app_context():
-        if db.engine.url.drivername == "sqlite":
-            migrate.init_app(app, db, render_as_batch=True)
-        else:
-            migrate.init_app(app, db)
+# def test_dashboard():
+#     app = Flask("learnpython")
+#     login = LoginManager(app)
+# 
+#     app.config.from_object(TestConfig)
+#     metadata = MetaData(naming_convention=convention)
+#     db = SQLAlchemy(app, metadata=metadata)
+#     migrate = Migrate(app, db)
+# 
+#     # this is required as SQLite does not support ALTER of constraints
+#     with app.app_context():
+#         if db.engine.url.drivername == "sqlite":
+#             migrate.init_app(app, db, render_as_batch=True)
+#         else:
+#             migrate.init_app(app, db)
+#     
+#     with app.test_client() as test_client:
+#         resp = test_client.get("/index")
+#         assert resp.status_code == 200
+#         print(resp.data)
